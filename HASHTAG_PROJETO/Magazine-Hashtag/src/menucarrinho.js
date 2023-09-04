fb138908e616ee3b39ec57bac92a0ec499867139
Rodrigo Barros <rodrigo.barros04@hotmail.com>
2023-09-04 20:06:05 -0300

    Update menucarrinho.js

--- HASHTAG_PROJETO/Magazine-Hashtag/src/menucarrinho.js
+++ HASHTAG_PROJETO/Magazine-Hashtag/src/menucarrinho.js
@@ -1,6 +1,6 @@
-import { catalogo } from "./utilidades";
+import { catalogo, salvarLocalStorage, lerLocalStorage } from "./utilidades";
 
-const idProdutoCarrinhoComQuantidade = {}
+const idsProdutoCarrinhoComQuantidade = lerLocalStorage("carrinho") ?? {};
 
 function abrirCarrinho() {
   document.getElementById("carrinho").classList.add("right-[0px]");
@@ -12,107 +12,144 @@ function fecharCarrinho() {
   document.getElementById("carrinho").classList.add("right-[-360px]");
 }
 
+function irParaCheckout() {
+  if (Object.keys(idsProdutoCarrinhoComQuantidade).length === 0) {
+    return;
+  }
+  window.location.href = "./checkout.html";
+}
+
 export function inicializarCarrinho() {
   const botaoFecharCarrinho = document.getElementById("fechar-carrinho");
   const botaoAbrirCarrinho = document.getElementById("abrir-carrinho");
+  const botaoIrParaCheckout = document.getElementById("finalizar-compra");
 
   botaoFecharCarrinho.addEventListener("click", fecharCarrinho);
   botaoAbrirCarrinho.addEventListener("click", abrirCarrinho);
+  botaoIrParaCheckout.addEventListener("click", irParaCheckout);
 }
 
-function removerDoCarrinho(idProdutos) {
-  delete idProdutoCarrinhoComQuantidade[idProdutos];
+function removerDoCarrinho(idProduto) {
+  delete idsProdutoCarrinhoComQuantidade[idProduto];
+  salvarLocalStorage("carrinho", idsProdutoCarrinhoComQuantidade);
+  atualizarPrecoCarrinho();
   renderizarProdutosCarrinho();
 }
 
-function incrementarQuantidadeProduto(idProdutos) {
-  idProdutoCarrinhoComQuantidade[idProdutos]++;
-  atualizarInformacaoQuantidade(idProdutos);
+function incrementarQuantidadeProduto(idProduto) {
+  idsProdutoCarrinhoComQuantidade[idProduto]++;
+  salvarLocalStorage("carrinho", idsProdutoCarrinhoComQuantidade);
+  atualizarPrecoCarrinho();
+  atualizarInformacaoQuantidade(idProduto);
 }
 
-function decrementarQuantidadeProduto(idProdutos) {
-    if(idProdutoCarrinhoComQuantidade[idProdutos] === 1){
-      removerDoCarrinho(idProdutos);
-      return;
-    }
-    idProdutoCarrinhoComQuantidade[idProdutos]--;
-    atualizarInformacaoQuantidade(idProdutos);
+function decrementarQuantidadeProduto(idProduto) {
+  if (idsProdutoCarrinhoComQuantidade[idProduto] === 1) {
+    removerDoCarrinho(idProduto);
+    return;
+  }
+  idsProdutoCarrinhoComQuantidade[idProduto]--;
+  salvarLocalStorage("carrinho", idsProdutoCarrinhoComQuantidade);
+  atualizarPrecoCarrinho();
+  atualizarInformacaoQuantidade(idProduto);
 }
 
-function atualizarInformacaoQuantidade(idProdutos) {
-  document.getElementById(`quantidade-${idProdutos}`).innerText = idProdutoCarrinhoComQuantidade[idProdutos];
+function atualizarInformacaoQuantidade(idProduto) {
+  document.getElementById(`quantidade-${idProduto}`).innerText =
+    idsProdutoCarrinhoComQuantidade[idProduto];
 }
 
-function desenhaProdutoNoCarrinho(idProdutos) {
-  const produto = catalogo.find((p) => p.id === idProdutos);
+function desenharProdutoNoCarrinho(idProduto) {
+  const produto = catalogo.find((p) => p.id === idProduto);
   const containerProdutosCarrinho =
     document.getElementById("produtos-carrinho");
 
-  const elementoArticle = document.createElement("article");
+  const elementoArticle = document.createElement("article"); //<article></article>
   const articleClasses = [
     "flex",
     "bg-slate-100",
-    "rounded-lg", 
+    "rounded-lg",
+    "p-1",
     "relative",
   ];
 
   for (const articleClass of articleClasses) {
     elementoArticle.classList.add(articleClass);
   }
+  //<article class="flex bg-slate-100 rounded-lg p-1 relative"></article>
 
-  const cartaoProdutoCarrinho = `<button id="remover-item-${produto.id}" class="absolute top-0 right-2">
-      <i 
+  const cartaoProdutoCarrinho = `<button id="remover-item-${
+    produto.id
+  }" class="absolute top-0 right-2">
+      <i
         class="fa-solid fa-circle-xmark text-slate-500 hover:text-slate-800"
       ></i>
     </button>
-    <img 
-      src="./assets/img/${produto.imagem}" 
+    <img
+      src="./assets/img/${produto.imagem}"
       alt="Carrinho: ${produto.nome}"
       class="h-24 rounded-lg"
     />
-   <div class="p-2 flex flex-col justify-between">
-     <p class="text-slate-900 text-sm">${produto.nome}</p>
-     <p class="text-slate-400 text-xs">Tamanho: M</p>
-     <p class="text-green-700 text-lg">$${produto.preco}</p>
-   </div>
-   <div class="flex text-slate-950 items-end absolute bottom-0 right-2 text-lg">
-    <button id="decrementar-produto-${produto.id}">-</button>
-    <p id="quantidade-${produto.id}" class="ml-2">${idProdutoCarrinhoComQuantidade[produto.id]}</p>
-    <button class="ml-2" id="incrementar-produto-${produto.id}">+</button>
-   </div>`;
+    <div class="p-2 flex flex-col justify-between">
+      <p class="text-slate-900 text-sm">
+        ${produto.nome}
+      </p>
+      <p class="text-slate-400 text-xs">Tamanho: M</p>
+      <p class="text-green-700 text-lg">$${produto.preco}</p>
+    </div>
+    <div class='flex text-slate-950 items-end absolute bottom-0 right-2 text-lg'>
+        <button id='decrementar-produto-${produto.id}'>-</button>
+        <p id='quantidade-${produto.id}' class='ml-2'>${
+    idsProdutoCarrinhoComQuantidade[produto.id]
+  }</p>
+        <button class='ml-2' id='incrementar-produto-${produto.id}'>+</button>
+    </div>`;
+  //<article class="flex bg-slate-100 rounded-lg p-1 relative">codigo do cartao do produto</article>
 
   elementoArticle.innerHTML = cartaoProdutoCarrinho;
   containerProdutosCarrinho.appendChild(elementoArticle);
-  
-  document
-  .getElementById(`decrementar-produto-${produto.id}`)
-  .addEventListener("click", () => decrementarQuantidadeProduto(produto.id));
 
   document
-  .getElementById(`incrementar-produto-${produto.id}`)
-  .addEventListener("click", () => incrementarQuantidadeProduto(produto.id));
+    .getElementById(`decrementar-produto-${produto.id}`)
+    .addEventListener("click", () => decrementarQuantidadeProduto(produto.id));
 
   document
-  .getElementById(`remover-item-${produto.id}`)
-  .addEventListener("click", () => removerDoCarrinho(produto.id));
+    .getElementById(`incrementar-produto-${produto.id}`)
+    .addEventListener("click", () => incrementarQuantidadeProduto(produto.id));
+
+  document
+    .getElementById(`remover-item-${produto.id}`)
+    .addEventListener("click", () => removerDoCarrinho(produto.id));
 }
 
-function renderizarProdutosCarrinho() {
+export function renderizarProdutosCarrinho() {
   const containerProdutosCarrinho =
-  document.getElementById("produtos-carrinho");
+    document.getElementById("produtos-carrinho");
   containerProdutosCarrinho.innerHTML = "";
 
-  for (const idProdutos in idProdutoCarrinhoComQuantidade) {
-    desenhaProdutoNoCarrinho(idProdutos);
+  for (const idProduto in idsProdutoCarrinhoComQuantidade) {
+    desenharProdutoNoCarrinho(idProduto);
   }
 }
 
-export function adicionarAoCarrinho(idProdutos) {
-    if (idProdutos in idProdutoCarrinhoComQuantidade){
-      incrementarQuantidadeProduto(idProdutos);
-      return;
-    }
-  idProdutoCarrinhoComQuantidade[idProdutos] = 1;
-  desenhaProdutoNoCarrinho(idProdutos);
-  
+export function adicionarAoCarrinho(idProduto) {
+  if (idProduto in idsProdutoCarrinhoComQuantidade) {
+    incrementarQuantidadeProduto(idProduto);
+    return;
+  }
+  idsProdutoCarrinhoComQuantidade[idProduto] = 1;
+  salvarLocalStorage("carrinho", idsProdutoCarrinhoComQuantidade);
+  desenharProdutoNoCarrinho(idProduto);
+  atualizarPrecoCarrinho();
+}
+
+export function atualizarPrecoCarrinho() {
+  const precoCarrinho = document.getElementById("preco-total");
+  let precoTotalCarrinho = 0;
+  for (const idProdutoNoCarrinho in idsProdutoCarrinhoComQuantidade) {
+    precoTotalCarrinho +=
+      catalogo.find((p) => p.id === idProdutoNoCarrinho).preco *
+      idsProdutoCarrinhoComQuantidade[idProdutoNoCarrinho];
+  }
+  precoCarrinho.innerText = `Total: $${precoTotalCarrinho}`;
 }
