28bac1d3abc5bacf6d3a26f8d44bdd23311fb894
Rodrigo Barros <rodrigo.barros04@hotmail.com>
2023-09-04 20:05:49 -0300

    Update main.js

--- HASHTAG_PROJETO/Magazine-Hashtag/main.js
+++ HASHTAG_PROJETO/Magazine-Hashtag/main.js
@@ -1,5 +1,14 @@
 import { renderizarCatalogo } from "./src/cartaoProduto";
-import { inicializarCarrinho } from "./src/menucarrinho";
+import { inicializarFiltros } from "./src/filtrosCatalogo";
+import {
+  atualizarPrecoCarrinho,
+  inicializarCarrinho,
+  renderizarProdutosCarrinho,
+} from "./src/menuCarrinho";
 
 renderizarCatalogo();
 inicializarCarrinho();
+renderizarProdutosCarrinho();
+atualizarPrecoCarrinho();
+inicializarFiltros();
+inicializarFiltros;
